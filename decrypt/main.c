6e78e5f9a48578a5aa4ab15d2340332912ca816d
tl <tom.f.lynch@gmail.com>
2021-12-07 11:29:31 -0800

    Changed keygen diagnostic output

---
+++ decrypt/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  decrypt
+//
+//  Created by Thomas Lynch on 12/6/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
